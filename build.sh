a6cc77bde4cedc3c4f816a4e49b438ac71a4176c
danijeljw-rpc <danijel@repasscloud.com>
2022-11-06 16:05:26 +1100

    update branch b8c790d3

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker rmi aps
 docker build --rm -t aps .
-docker run --rm --name aps -d -p 8888:80 aps
+docker run --rm --name aps -d -p 4200:4200 aps ng serve --proxy-config proxy.conf.json --host 0.0.0.0 --port 4200 --disable-host-check
